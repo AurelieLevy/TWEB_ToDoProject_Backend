38106446bc3bb50091131d8153a64056f7278985
Rémi Jacquemard <remi.jacquemard@heig-vd.ch>
2018-01-16 22:11:18 +0100

    buying image seems to work

--- src/dbservice.js
+++ src/dbservice.js
@@ -7,15 +7,49 @@ mongoose.Promise = require('bluebird');
 // On local docker :
 const url = 'mongodb://192.168.99.100/todoproject'
 
+// arrays defining images id avaiable;
+const images = [
+    {imageId:1, value:5},
+    {imageId:2, value:20},
+    {imageId:3, value:2},
+    {imageId:4, value:8},
+];
+
 // ---------- SCHEMA AND MODELS ----------
-const userSchema = mongoose.Schema({
-    wunderlistId:Number,
-    gold:Number,
-    ownedImages:[{pathId:String}]
+let userSchema = mongoose.Schema({
+    wunderlistId: Number,
+    gold: Number,
+    ownedImages: [{ imageId: String, value: Number }]
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.methods.addGold = function (value) {
+    this.gold += value;    
+}
 
+userSchema.methods.getAvaiableImages = function () {
+    // Removing the images already owned from the full list
+    // https://stackoverflow.com/questions/5767325/how-do-i-remove-a-particular-element-from-an-array-in-javascript
+    return images.filter(item => !this.ownedImages.map(i => i.imageId).includes(item));
+}
+
+userSchema.methods.buyImage = function (id) {
+    if(this.ownedImages.map(i => i.imageId).includes(id))
+        return; // Already owned
+
+    // Getting the image
+    let image = images.find(i => i.imageId == id);
+
+    if(this.gold < image.value)
+        return; // Not enough money
+
+    // Effectively buying the image
+    this.gold -= image.value;
+    this.ownedImages.push(image);
+    console.log(image);
+}
+
+// Creating the model
+const User = mongoose.model('User', userSchema);
 
 class DBService {
     constructor() {
@@ -32,13 +66,13 @@ class DBService {
     }
 
     createOrGetUserPromise(userId) {
-        return User.findOne({wunderlistId:userId})
+        return User.findOne({ wunderlistId: userId })
             .then((user) => {
-                if(user == null){
+                if (user == null) {
                     // Creating a new user if not exists
                     user = new User({
-                        wunderlistId:userId, 
-                        gold:0
+                        wunderlistId: userId,
+                        gold: 0
                     });
                     user.save();
                 }
--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ const service = new DBService();
 service.connect()
     .then(() =>  service.createOrGetUserPromise(145))
     .then((user) => {
-        console.log(user);
+        //console.log(user);
+        user.addGold(50);
+        let u = user.getAvaiableImages();
+        //console.log(u);
+        console.log(user.ownedImages);
+        user.buyImage(1);
+        console.log(user.ownedImages);
     });
